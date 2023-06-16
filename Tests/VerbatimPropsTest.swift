24edfc8169fc9cde2aa5b195749d8984e6ebdf0b
Zeio Nara <zeionara@gmail.com>
2023-06-17 02:52:12 +0300

    feat(test): added more verbatim props tests

--- Tests/VerbatimPropsTest.swift
+++ Tests/VerbatimPropsTest.swift
@@ -17,27 +17,67 @@ struct ConfigSpec {
     }
 }
 
-private let dummy = ConfigSpec(
-    config: """
-    foo:
-        verbatim: true
-        value: bar
-    """,
-    env: """
-    """,
-    reader: """
-    export const config = {
-      foo: bar
-    }
-    """
-)
 
 final class VerbatimPropsTest: XCTestCase {
-    func testConfig () throws {
-        try dummy.test()
-        // let config = try Config(parsing: configDummy)
+    func testDummyConfig () throws {
+        try ConfigSpec(
+            config: """
+            foo:
+                verbatim: true
+                value: bar
+            """,
+            env: """
+            """,
+            reader: """
+            export const config = {
+              foo: bar
+            }
+            """
+        ).test()
+    }
 
-        // try XCTAssertEqual(config.toString(), envDummy)
-        // try XCTAssertEqual(config.toStringReader(as: .js), readerDummy)
+    func testTransparency () throws {
+        try ConfigSpec(
+            config: """
+            foo:
+                verbatim: false
+                value:
+                  - bar
+                  - baz
+            """,
+            env: """
+            FOO=bar,baz
+            """,
+            reader: """
+            export const config = {
+              foo: process.env.FOO
+            }
+            """
+        ).test()
+    }
+
+    func testNesting () throws {
+        try ConfigSpec(
+            config: """
+            foo:
+                verbatim: true
+                value:
+                    verbatim: true
+                    bar:
+                        baz: qux
+            """,
+            env: """
+            """,
+            reader: """
+            export const config = {
+              foo: {
+                bar: {
+                  baz: qux
+                },
+                verbatim: true
+              }
+            }
+            """
+        ).test()
     }
 }
